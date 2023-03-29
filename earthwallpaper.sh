01034c5955b9fbde5233f0b38217768f7c771735
smthzch <ezke.smith@gmail.com>
2023-03-29 08:16:40 -0400

    add dbus address

--- earthwallpaper.sh
+++ earthwallpaper.sh
@@ -10,6 +10,9 @@ curl -s https://cdn.star.nesdis.noaa.gov/GOES16/ABI/FD/GEOCOLOR/1808x1808.jpg > 
 GIO_EXTRA_MODULES="/usr/lib/x86_64-linux-gnu/gio/modules/"
 export GIO_EXTRA_MODULES
 
+PID=$(pgrep gnome-session | head -n 1)
+export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
+
 # set standard and dark theme background
 gsettings set org.gnome.desktop.background picture-options "scaled"
 gsettings set org.gnome.desktop.background picture-uri-dark file://"$img_pth"
